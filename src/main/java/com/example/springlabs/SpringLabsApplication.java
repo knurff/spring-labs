7f71ad214bde8ff247247315f3b6f4134a5cd60e
knurff <beryndaviktor@gmail.com>
2023-10-02 23:38:02 +0300

    Initial commit

---
+++ src/main/java/com/example/springlabs/SpringLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringLabsApplication.class, args);
+  }
+
+}
