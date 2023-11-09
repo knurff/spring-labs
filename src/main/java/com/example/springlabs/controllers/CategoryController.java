3531e239c1fa1bc296850fef4a6bd6800bc6041b
xd6d <kurkin566@gmail.com>
2023-11-10 01:01:42 +0200

    fixed imports

--- src/main/java/com/example/springlabs/controllers/CategoryController.java
+++ src/main/java/com/example/springlabs/controllers/CategoryController.java
@@ -23,7 +23,15 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.HandlerMapping;
 
 @Tag(name = "Category", description = "Operations related to categories")
